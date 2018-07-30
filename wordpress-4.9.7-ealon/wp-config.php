628564ac1bda572a60b84e28b3943e086a396461
Yilong Huang <ealonhuang@gmail.com>
2018-07-30 23:05:06 +1000

    - wordpress configuration

--- wordpress-4.9.7-ealon/wp-config.php
+++ wordpress-4.9.7-ealon/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'ealon_wordpress_theme');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'ealon');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'wg448pJtpZCDejs5');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         '+`TYhNNs)l-|KyW%hM`]+|}dU=-WdF}k-=8[6U{`zvJg#T%)w)nRQMgT4^=;&zX5');
+define('SECURE_AUTH_KEY',  'E+n`D/$mSzt>,sS^io{pW*#fNhk-AN?CW)@A<gr)D#/XD%.@0%F&aLs[_I(V4q+o');
+define('LOGGED_IN_KEY',    '2/2, QO!-+_%,#9m[-!e.DQ$3,)=OT:$]M2AN?OMNKt|@*}CxP|0u@uWOby4e:!4');
+define('NONCE_KEY',        ']c~r:qww-8w3<DQKY#M:1~wb&UNu,Qaf,2vd`u IQ|Ke-/uM9fbL9DUBx@5LX9,r');
+define('AUTH_SALT',        '74B2WTch4= 1Z^6Z^dgV.-PGZ`TY%a4-%V&H|,QH`k/j=RRP} F7NN!hnP^enOP-');
+define('SECURE_AUTH_SALT', '),-g}zVx$=2<3DbFMA/>R.qJS`M/Nt4HiW-N&xPf{s5)XBjH!3MJGci@g:B@[>|&');
+define('LOGGED_IN_SALT',   ')p#T7SgB*+N<;#?,T/khOrCCad!HwIF3s+A&,nnaCfyy2WkJ:@Z+}@zDK{t-KYl@');
+define('NONCE_SALT',       'hMDy-HE7&t^DqaVK CVV|Oc>5~Yqe=Sh[QQ{2(4<g%kI1vU{su1W8 *T}T_{RHPp');
 
 /**#@-*/
 
@@ -63,7 +63,7 @@ define('NONCE_SALT',       'put your unique phrase here');
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = 'ealon_wp_';
 
 /**
  * For developers: WordPress debugging mode.
